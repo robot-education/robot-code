0343b7e4993c28c43cdf31cc6e74257285cb1340
Alex Kempen <alex.bookreader@gmail.com>
2023-04-25 06:46:47 +0000

    Continued to work on robot tube

--- fs_gen/robot_tube_ui.py
+++ fs_gen/robot_tube_ui.py
@@ -2,6 +2,27 @@ from library import *
 
 studio = Studio("robotTubeUi.gen.fs")
 
+wall_thickness = (
+    studio.register(Enum("WallThickness"))
+    .add_value("ONE_SIXTEENTH", user_name='1/16"')
+    .add_value("ONE_EIGHTH", user_name='1/8"')
+    .add_value("CUSTOM")
+)
+
+custom_wall_thickness = studio.register(
+    UiTestPredicate("isCustomWallThickness", equal(wall_thickness.CUSTOM))
+).call()
+
+wall_thickness_predicate = studio.register(UiPredicate("wallThickness"))
+wall_thickness_predicate.add(
+    EnumAnnotation(
+        wall_thickness, ui_hints=[UiHint.REMEMBER_PREVIOUS_VALUE, UiHint.SHOW_LABEL]
+    )
+)
+wall_if = wall_thickness_predicate.register(If(custom_wall_thickness)).add(
+    LengthAnnotation("wallThickness", bound_spec=LengthBound.SHELL_OFFSET_BOUNDS)
+)
+
 tube_size = (
     studio.register(Enum("TubeSize"))
     .add_value("ONE_BY_ONE", user_name="1x1")
@@ -21,6 +42,7 @@ max_tube_type = (
     .add_value("GRID")
     .add_value("MAX", user_name="MAX")
 )
+
 
 can_be_light = (
     studio.register(
@@ -43,13 +65,11 @@ size_predicates = [
     for value in tube_size
 ]
 
-# studio.register(
-#     UiTestPredicate("isMaxTube", size_predicates[0])
-# )
+studio.register(UiTestPredicate("isMaxTube", ~size_predicates[2] & type_predicates[0]))
 
 
-tube_ui = studio.register(UiPredicate("robotTube"))
-tube_ui.add(
+tube_predicate = studio.register(UiPredicate("tubeSize"))
+tube_predicate.add(
     EnumAnnotation(
         tube_size,
         default="TWO_BY_ONE",
@@ -57,11 +77,9 @@ tube_ui.add(
     )
 )
 
-tube_if = tube_ui.register(If(size_predicates[2]))
+tube_if = tube_predicate.register(If(size_predicates[2]))
 tube_if.add(LengthAnnotation("length", bound_spec=LengthBound.LENGTH_BOUNDS))
 tube_if.add(LengthAnnotation("width", bound_spec=LengthBound.LENGTH_BOUNDS))
-
-# inner_if = inner_if.else_if(equal(tube_type.CUSTOM))
 
 tube_if = tube_if.or_else()
 tube_if.add(
@@ -69,7 +87,7 @@ tube_if.add(
         tube_type,
         default="CUSTOM",
         ui_hints=[UiHint.REMEMBER_PREVIOUS_VALUE, UiHint.SHOW_LABEL],
-    ),
+    )
 )
 
 inner_if = tube_if.register(If(size_predicates[1] & type_predicates[0])).add(
@@ -80,9 +98,19 @@ inner_if.register(If(can_be_light)).add(
     BooleanAnnotation("isLight", user_name="Light"),
 )
 
+wall_thickness_if = tube_predicate.register(If(size_predicates[2] | type_predicates[1]))
+wall_thickness_if.add(wall_thickness_predicate.call())
 
-tube_ui.register(If(size_predicates[2] | type_predicates[1])).add(
-    LengthAnnotation("wallThickness", bound_spec=LengthBound.SHELL_OFFSET_BOUNDS)
+fit = studio.register(Enum("HoleFit")).add_value("CLOSE").add_value("FREE")
+size = (
+    studio.register(Enum("HoleSize"))
+    .add_value("NO_8", user_name="#8")
+    .add_value("NO_10", user_name="#10")
+    .add_value("CUSTOM")
 )
+
+hole_predicate = studio.register(UiPredicate("tubeHole"))
+# hole_predicate.register(If())
+
 
 studio.print()
