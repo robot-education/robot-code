1a92cdbc05ebf4571524cd8296e27c219959e566
Alex Kempen <alex.bookreader@gmail.com>
2024-07-10 14:58:09 -0500

    Updated vscode settings for latest version

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,7 +13,7 @@ import "vite/modulepreload-polyfill";
 import "normalize.css/normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-import "@blueprintjs/select/lib/css/blueprint-select.css";
+// import "@blueprintjs/select/lib/css/blueprint-select.css";
 
 // Custom css
 import "./main.scss";
